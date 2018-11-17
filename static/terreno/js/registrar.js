de5bf8ee469813ebaea18c250d4028120753fea3
Carlos-san <cm.090691@gmail.com>
2018-11-17 15:20:37 -0500

    Ajusta visualización de medidas en tabla de seguimiento, registro y actualización

--- static/terreno/js/registrar.js
+++ static/terreno/js/registrar.js
@@ -79,7 +79,7 @@ var registrarTerreno = function () {
                 area = google.maps.geometry.spherical.computeArea(this.poligono.getPath());
                 perimetro = google.maps.geometry.spherical.computeLength(this.poligono.getPath());
                 $("#areaPoligonoLbl").html(" " + area.toFixed() + " metros cuadrados");
-                $("#perimetroPoligonoLbl").html(" " + perimetro.toFixed() + " metros cuadrados");
+                $("#perimetroPoligonoLbl").html(" " + perimetro.toFixed() + " metros");
             }
         },
         registrarPoligono: function (municipio) {
