761a153ef04e0cc5282f865ca558be42866baf64
Daniel Muñoz <dani_mm@live.cl>
2018-11-17 12:08:12 -0500

    Update tests.py

--- apps/terreno/tests.py
+++ apps/terreno/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
 
+from .models import Poligono
+from .models import PuntoSiembra
+from .models import Siembra
+
 # Create your tests here.
+class PoligonoTests(TestCase):
+
+    def setUp(self):
+        print("Prueba Unitaria Poligono")
+        if (Poligono.objects.create(nombre='Villavicencio',coordenadas_puntos = "24.000", tipo_patron = 1, municipio = 1, usuario = 1)):
+            print("Exitoso")
+        else:
+            print("No Exitoso")
+
+    def test_text_content(self):
+        poligono = Poligono.objects.get(id=1)
+        print(poligono.nombre)
+        print(poligono.coordenadas_puntos)
+     
+# Create your tests here.
+class PuntoSiembraTests(TestCase):
+
+    def setUp(self):
+        print("Prueba Unitaria PuntoSiembra")
+        if (PuntoSiembra.objects.create(nombre='Villavicencio',latitud = "24.000",longitud = "24.000", poligono = 1, especie = 1)):
+            print("Exitoso")
+        else:
+            print("No Exitoso")
+
+    def test_text_content(self):
+        puntoSiembra = PuntoSiembra.objects.get(id=1)
+        print(puntoSiembra.nombre)
+        print(puntoSiembra.longitd)
+        print(puntoSiembra.latitud)
+        
+# Create your tests here.
+class SiembraTests(TestCase):
+
+    def setUp(self):
+        print("Prueba Unitaria Siembra")
+        if (Siembra.objects.create(temperatura='Villavicencio',altitud = "24.000",humedad = "24.000", punto_siembra = 1, donacion = 1)):
+            print("Exitoso")
+        else:
+            print("No Exitoso")
+
+    def test_text_content(self):
+        siembra = Siembra.objects.get(id=1)
+        print(siembra.temperatura)
+        print(siembra.humedad)
+        print(siembra.altitud)
