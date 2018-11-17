f52d0edceb68a7cb019fd7688ff1a39099cf877b
oabaquero <obaquerog@gmail.com>
2018-11-17 11:16:48 -0500

    Habilita la opción de eliminar un terreno

--- apps/terreno/urls.py
+++ apps/terreno/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.urls import path
 from django.contrib import admin
-from apps.terreno.views import index, registro, registro_poligono
+from apps.terreno.views import index, registro, registro_poligono, eliminar_terreno
 
 urlpatterns = [
     path('seguimiento', index, name='terreno_seguimiento'),
     path('registro', registro, name='terreno_registro'),
     path('registrar_poligono', registro_poligono, name='terreno_registrar_poligono'),
+    path('eliminar_terreno', eliminar_terreno, name='eliminar_terreno'),
 ]
